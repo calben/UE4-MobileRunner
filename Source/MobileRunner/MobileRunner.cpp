7c1392ac844c71ffa757b152f737276780002853
Calem Bendell <calem.j.bendell@gmail.com>
2016-10-31 20:57:02 -0400

    add initial game files

---
+++ Source/MobileRunner/MobileRunner.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MobileRunner.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MobileRunner, "MobileRunner" );
